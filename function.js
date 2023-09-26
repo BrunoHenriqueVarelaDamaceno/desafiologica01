63957adcd093b4044535c5d7ac31efa5e842c3d4
Bruno Henrique Varela Damaceno <90577862+BrunoHenriqueVarelaDamaceno@users.noreply.github.com>
2023-09-26 20:31:14 -0300

    function.js

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
-let numberOne = Number(prompt('Digite o primeiro número'))
-let numberTwo = Number(prompt('Digite o segundo número'))
+let numberOne = Number(prompt('Digite o primeiro número:'))
+let numberTwo = Number(prompt('Digite o segundo número:'))
 
-alert(`A soma dos dois números é ${numberOne + numberTwo}`)
-alert(`A subtração dos dois números é ${numberOne - numberTwo}`)
-alert(`A multiplicação dos dois números é ${numberOne * numberTwo}`)
-alert(`A divisão dos dois números é ${numberOne / numberTwo}`)
-alert(`O resto da divisão dos dois números é ${numberOne % numberTwo}`)
+alert(`A soma dos dois números é ${numberOne + numberTwo}.`)
+alert(`A subtração dos dois números é ${numberOne - numberTwo}.`)
+alert(`A multiplicação dos dois números é ${numberOne * numberTwo}.`)
+alert(`A divisão dos dois números é ${numberOne / numberTwo}.`)
+alert(`O resto da divisão dos dois números é ${numberOne % numberTwo}.`)
 
 let result = numberOne + numberTwo
 
@@ -16,7 +16,7 @@ if(result % 2 == 0) {
 }
 
 if(numberOne == numberTwo) {
-  alert('Os números inseridos são iguais')
+  alert('Os números inseridos são iguais.')
 } else {
-  alert('Os números inseridos são diferentes')
+  alert('Os números inseridos são diferentes.')
 }
